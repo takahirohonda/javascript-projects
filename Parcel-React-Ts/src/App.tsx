4489c0afbaff9877442efbff5f37c640bda6d4c1
mydatahack <thon6thon6@hotmail.com>
2021-04-24 12:26:40 +1000

    Initial commit

---
+++ Parcel-React-Ts/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Header from './components/Header';
+import './fonts/Lato/Lato.css';
+
+const App = () => (
+  <Header>Hello World</Header>
+);
+
+export default App;
