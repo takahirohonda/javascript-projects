6173807217a8bbab5ab1c3ce7d74be54fd833fd9
mydatahack <thon6thon6@hotmail.com>
2021-04-29 18:23:52 +1000

    Fix dodgy css import in parcel-react-ts

--- Parcel-React-Ts/src/App.tsx
+++ Parcel-React-Ts/src/App.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
 
-// These css import will be compiled into a single file and injected through the link tag.
-import './css/global'; 
-import './fonts/Lato/Lato.css';
-
 const App = () => (
   <Header>Hello World</Header>
 );
