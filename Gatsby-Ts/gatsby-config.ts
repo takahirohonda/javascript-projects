0f11b5b7d7dd5bc03e0476da57ad52405143a2f7
mydatahack <thon6thon6@hotmail.com>
2022-03-19 17:03:31 +1100

    Spell fix

--- Gatsby-Ts/gatsby-config.ts
+++ Gatsby-Ts/gatsby-config.ts
@@ -1,12 +1,10 @@
-// Defines site's metatdata, plugins, other general config
+// Defines site's metadata, plugins, other general config
 // https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
 
-// This is used only for getting gatsby-plutgin-ts-config setup.
+// This is used only for getting gatsby-plugin-ts-config setup.
 // One JS file is required to have the plugin setup.
 // The rest can be typescripts
 
 module.exports = {
-  plugins: [
-    'gatsby-plugin-scss-typescript',
-  ],
+  plugins: ["gatsby-plugin-scss-typescript"],
 };
