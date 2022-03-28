b83c2c4c14d66d38e03add7eac7e63ba4151b7e8
mydatahack <43569574+mydatahack@users.noreply.github.com>
2022-03-28 11:10:53 +1100

    Upgrade parcel version and install custom eslint (#16)
    * Upgrade parcel version and install custom eslint
    
    * remove-eslint-from-dependencies

--- Parcel-React-Ts/src/index.tsx
+++ Parcel-React-Ts/src/index.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import { render } from 'react-dom';
+import { render } from 'react-dom'
 // These css import will be compiled into a single file and injected through the link tag.
-import './css/global.scss';
-import './fonts/Lato/Lato.css';
+import './css/global.scss'
+import './fonts/Lato/Lato.css'
 /* eslint-disable max-len */
 // Importing App after CSS import. This ensures all the CSS from the components come after global and font css
 // When it gets compiled into a single file.
-import App from './App';
+import App from './App'
 
-render(<App />, document.getElementById('app'));
+render(<App />, document.getElementById('app'))
