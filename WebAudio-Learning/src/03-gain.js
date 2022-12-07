283b1bfd223bf31e9c0f2fd3337a14e4feceffdd
mydatahack <thon6thon6@hotmail.com>
2022-12-08 08:19:15 +1100

    complete section 2 Web Audio API

---
+++ WebAudio-Learning/src/03-gain.js
@@ -0,0 +1,80 @@
+let audioContext;
+let audio;
+let gainNode;
+
+function mousePressed() {
+  if (!audioContext) {
+    audioContext = new AudioContext();
+    audio = document.createElement('audio');
+    audio.src = 'audio/piano.mp3';
+
+    // To play audio through Glitch.com CDN
+    audio.crossOrigin = 'Anonymous';
+
+    audio.loop = true;
+    audio.play();
+
+    // Create a "Media Element" source node
+    const source = audioContext.createMediaElementSource(audio);
+
+    // Create a gain for volume adjustment
+    gainNode = audioContext.createGain();
+
+    // wire source to gain
+    source.connect(gainNode);
+
+    // wire the gain -> speaker
+    gainNode.connect(audioContext.destination);
+  } else {
+    // Clean up our element and audio context
+    audio.pause();
+    audioContext.close();
+    audioContext = audio = null;
+  }
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  // fill background
+  background('black');
+
+  fill('white');
+  noStroke();
+
+  // Draw play/pause button
+  const dim = min(width, height);
+  if (audioContext) {
+    // Get a new volume based on mouse position
+    const volume = abs(mouseX - width / 2) / (width / 2);
+
+    // Schedule a gradual shift in value with a small time constant
+    // if we do this, gainNode.gain.value = volume, it will have a pop sound because the change is too sudden.
+    // The best practise is to shift gradually from one value to another.
+    gainNode.gain.setTargetAtTime(volume, audioContext.currentTime, 0.01);
+
+    // Draw a volume meter
+    rectMode(CENTER);
+    rect(width / 2, height / 2, dim * volume, dim * 0.05);
+  } else {
+    polygon(width / 2, height / 2, dim * 0.1, 3);
+  }
+}
+
+// Draw a basic polygon, handles triangles, squares, pentagons, etc
+function polygon(x, y, radius, sides = 3, angle = 0) {
+  beginShape();
+  for (let i = 0; i < sides; i++) {
+    const a = angle + TWO_PI * (i / sides);
+    let sx = x + cos(a) * radius;
+    let sy = y + sin(a) * radius;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
+}
